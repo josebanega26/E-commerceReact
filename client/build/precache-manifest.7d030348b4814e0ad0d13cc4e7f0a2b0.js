18faa708abf7bc3ae76b5d6e6ecd7749454be76d
Jose Banega <banegajose26@gmail.com>
2020-02-16 01:29:44 -0400

    Fixed git Ignore

---
+++ client/build/precache-manifest.7d030348b4814e0ad0d13cc4e7f0a2b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b8e21bec2fd938f21950d903383c3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e138e33aa2c6b5eed473",
+    "url": "/static/css/2.7fc0138c.chunk.css"
+  },
+  {
+    "revision": "edd3247473741db6f5d6",
+    "url": "/static/css/main.36d730c3.chunk.css"
+  },
+  {
+    "revision": "e138e33aa2c6b5eed473",
+    "url": "/static/js/2.d5d4a553.chunk.js"
+  },
+  {
+    "revision": "eaded8898e412025f59bd07d75644ca4",
+    "url": "/static/js/2.d5d4a553.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edd3247473741db6f5d6",
+    "url": "/static/js/main.db914cda.chunk.js"
+  },
+  {
+    "revision": "e8abe93357e4d0c80bdd",
+    "url": "/static/js/runtime-main.ed56c59f.js"
+  },
+  {
+    "revision": "e2d6181871601ba68805f36fd551ab83",
+    "url": "/static/media/logo.e2d61818.svg"
+  }
+]);
