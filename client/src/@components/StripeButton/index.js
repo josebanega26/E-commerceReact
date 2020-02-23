4a2e1f7b645f8e735c005c1013092b78942af105
Jose Banega <banegajose26@gmail.com>
2020-02-23 00:51:42 -0400

    add backend to crw-electronic

--- client/src/@components/StripeButton/index.js
+++ client/src/@components/StripeButton/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 import logo from '../../@assets/logo.svg'
+import axios from 'axios'
 const StripeButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publicKey = 'pk_test_i8aETzzPuCfUnZFg3BaVZ2y800wv4s3UW1'
     const onToken = token => {
-        alert('Payment successfull')
+        alert('Payment successful')
+        axios({
+            url: 'payment',
+            method: 'POST',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(res => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.error(error);
+            alert('Problem with the payment')
+        })
     }
     return (
 
--- server.js
+++ server.js
@@ -4,3 +4,39 @@ const bodyParser = require('body-parser');
 const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/client/build")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+    })
+}
+
+app.listen(port, (error) => {
+    if (error) throw error;
+    console.log('Server run on port:', port)
+})
+
+app.post('/payment', (req, res) => {
+    console.log('req', req)
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: req.body.currency || 'usd'
+    }
+
+    stripe.charges.create(body, (stripeError, stripeResponse) => {
+        if (stripeError) {
+            res.status('500').send({ err: stripeError })
+        } else {
+            res.status('200').send({ success: stripeResponse })
+        }
+    })
+})
